181c94971bab01d87cd9bc16d10b4372c72fdd57
Goulven Le Breton <goulven.lebreton+github@gmail.com>
2020-02-28 12:23:32 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lpr-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'lpr-front';
+}
