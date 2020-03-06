7c184d67b852309d4bf86463c5beea0c6c4c597c
Valentin GOT <vgot@sii.fr>
2020-03-06 15:24:22 +0100

    style(adherent): new style

--- src/app/adherent/adherent.module.ts
+++ src/app/adherent/adherent.module.ts
@@ -1,14 +1,16 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
-import {SharedModule} from '../shared/shared.module';
-import {AdherentComponent} from './adherent.component';
-import {AdherentsTableComponent} from './adherents-table/adherents-table.component';
+import { SharedModule } from '../shared/shared.module';
+import { AdherentComponent } from './adherent.component';
+import { AdherentsTableComponent } from './adherents-table/adherents-table.component';
 import { AdherentRoute } from './adherent.routing';
 
 @NgModule({
@@ -19,6 +21,8 @@ import { AdherentRoute } from './adherent.routing';
     MatInputModule,
     MatTableModule,
     MatPaginatorModule,
+    MatIconModule,
+    MatButtonModule,
     SharedModule,
     AdherentRoute
   ],
